1a839abb24e3fb7578cc6c7ee617986ff97a74fa
Benjamin Gudehus <hastebrot@gmail.com>
2016-02-13 14:08:34 +0100

    Add example with property() and lazy().

--- src/main/java/tornadofx/Properties.kt
+++ src/main/java/tornadofx/Properties.kt
@@ -5,6 +5,7 @@ import javafx.beans.property.SimpleObjectProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+fun <T> property(fxProperty: Property<T>) = PropertyDelegate(fxProperty)
 fun <T> property(value: T? = null) = PropertyDelegate(SimpleObjectProperty<T>(value))
 
 class PropertyDelegate<T>(private val fxProperty: Property<T>) : ReadWriteProperty<Any?, T> {
--- src/test/java/tornadofx/PropertiesTest.kt
+++ src/test/java/tornadofx/PropertiesTest.kt
@@ -1,30 +1,46 @@
 package tornadofx
 
+import javafx.beans.property.SimpleObjectProperty
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class PropertiesTest {
 
-    internal class Fixtures {
-        val string by property<String>()
-        var stringVar by property<String>()
-    }
-
     @Test
     fun `should get string property`() {
-        val fixtures = Fixtures()
+        // given:
+        val fixture = object {
+            val string by property<String>()
+        }
 
         // expect:
-        assertEquals(fixtures.string, null)
+        assertEquals(fixture.string, null)
     }
 
     @Test
     fun `should set string property`() {
-        val fixtures = Fixtures()
-        fixtures.stringVar = "foo"
+        // given:
+        val fixture = object {
+            var string by property<String>()
+        }
 
-        // expect:
-        assertEquals(fixtures.stringVar, "foo")
+        // when:
+        fixture.string = "foo"
+
+        // then:
+        assertEquals(fixture.string, "foo")
+    }
+
+    @Test
+    fun `property() in combination with lazy()`() {
+        val fixture = object {
+            private val stringProperty by lazy {
+                println("in lazy lambda expr")
+                SimpleObjectProperty<String>()
+            }
+
+            val string by property(stringProperty)
+        }
     }
 
 }
