ac203d8371fbe6c5b3832b1218039a73055e219c
Benjamin Gudehus <hastebrot@gmail.com>
2016-02-15 16:33:39 +0100

    Change T to T? in PropertyDelegate.

--- src/main/java/tornadofx/Properties.kt
+++ src/main/java/tornadofx/Properties.kt
@@ -8,13 +8,13 @@ import kotlin.reflect.KProperty
 fun <T> property(value: T? = null) = PropertyDelegate(SimpleObjectProperty<T>(value))
 fun <T> property(block: () -> Property<T>) = PropertyDelegate(block())
 
-class PropertyDelegate<T>(private val fxProperty: Property<T>) : ReadWriteProperty<Any?, T> {
+class PropertyDelegate<T>(private val fxProperty: Property<T>) : ReadWriteProperty<Any?, T?> {
 
-    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T? {
         return fxProperty.value
     }
 
-    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T?) {
         fxProperty.value = value
     }
 
