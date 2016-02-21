ca19588870c1a837aa501cd1857da3e71194f1ef
Benjamin Gudehus <hastebrot@gmail.com>
2016-02-21 01:26:13 +0100

    Add initial tests for Builders.

---
+++ src/test/kotlin/tornadofx/BuildersTest.kt
@@ -0,0 +1,33 @@
+package tornadofx
+
+import javafx.scene.layout.StackPane
+import javafx.scene.text.Text
+import javafx.stage.Stage
+import org.hamcrest.Matchers
+import org.junit.Before
+import org.junit.Test
+import org.testfx.api.FxToolkit
+import org.testfx.api.FxAssert.verifyThat
+import org.testfx.matcher.base.NodeMatchers
+
+class BuildersTest {
+
+    val primaryStage: Stage = FxToolkit.registerPrimaryStage()
+
+    lateinit var pane: StackPane
+
+    @Before
+    fun setup() {
+        pane = StackPane()
+    }
+
+    @Test
+    fun text_builder() {
+        // expect:
+        verifyThat(pane.text(), Matchers.instanceOf(Text::class.java))
+        verifyThat(pane.text(), NodeMatchers.hasText(""))
+        verifyThat(pane.text("foo"), NodeMatchers.hasText("foo"))
+        verifyThat(pane.text() { text = "bar" }, NodeMatchers.hasText("bar"))
+    }
+
+}
