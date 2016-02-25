baf632c7f655621a9f55eafc8a85f43f5396b698
Thomas Nield <thomasnield@live.com>
2016-02-25 12:29:02 -0600

    implement SingleAssign functionality

--- src/main/java/tornadofx/Properties.kt
+++ src/main/java/tornadofx/Properties.kt
@@ -29,3 +29,66 @@ fun <T> Any.getProperty(prop: KMutableProperty1<*, T>): ObjectProperty<T> {
     val delegate = field.get(this) as PropertyDelegate<T>
     return delegate.fxProperty as ObjectProperty<T>
 }
+
+enum class SingleAssignThreadSafetyMode {
+    SYNCHRONIZED,
+    NONE
+}
+
+fun <T> singleAssign(threadSafeyMode: SingleAssignThreadSafetyMode = SingleAssignThreadSafetyMode.SYNCHRONIZED): SingleAssign<T> =
+    if (threadSafeyMode.equals(SingleAssignThreadSafetyMode.SYNCHRONIZED)) SynchronizedSingleAssign<T>() else UnsynchronizedSingleAssign<T>()
+
+private object UNINITIALIZED_VALUE
+
+interface SingleAssign<T> {
+    fun isInitialized(): Boolean
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T)
+}
+
+private class SynchronizedSingleAssign<T>: SingleAssign<T> {
+
+    @Volatile
+    private var initialized = false
+
+    @Volatile
+    private var _value: Any? = UNINITIALIZED_VALUE
+
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (!initialized)
+            throw Exception("Value has not been assigned yet!")
+        return _value as T
+    }
+
+    override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        synchronized(this) {
+            if (initialized) {
+                throw Exception("Value has already been assigned!")
+            }
+            _value = value as T
+            initialized = true
+        }
+    }
+    override fun isInitialized() = initialized
+}
+
+private class UnsynchronizedSingleAssign<T>: SingleAssign<T> {
+
+    private var initialized = false
+    private var _value: Any? = UNINITIALIZED_VALUE
+
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (!initialized)
+            throw Exception("Value has not been assigned yet!")
+        return _value as T
+    }
+
+    override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        if (initialized) {
+            throw Exception("Value has already been assigned!")
+        }
+        _value = value as T
+        initialized = true
+    }
+    override fun isInitialized() = initialized
+}
