b7f80b3067539a49e3d0045d848d83785fca6b15
Benjamin Gudehus <hastebrot@gmail.com>
2016-03-29 16:28:33 +0200

    Test: Add simple test for wiki example 1.

--- src/test/kotlin/tornadofx/BuildersTest.kt
+++ src/test/kotlin/tornadofx/BuildersTest.kt
@@ -1,14 +1,19 @@
 package tornadofx
 
+import javafx.geometry.Insets
+import javafx.scene.Scene
 import javafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
 import javafx.scene.text.Text
 import javafx.stage.Stage
 import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Test
 import org.testfx.api.FxAssert.verifyThat
+import org.testfx.api.FxRobot
 import org.testfx.api.FxToolkit
 import org.testfx.matcher.base.NodeMatchers
+import java.nio.file.Paths
 
 class BuildersTest {
 
@@ -28,6 +33,46 @@ class BuildersTest {
         verifyThat(pane.text(), NodeMatchers.hasText(""))
         verifyThat(pane.text("foo"), NodeMatchers.hasText("foo"))
         verifyThat(pane.text() { text = "bar" }, NodeMatchers.hasText("bar"))
+    }
+
+    @Test
+    fun wiki_example_1() {
+        class Example1 : View() {
+            override val root = VBox()
+
+            init {
+                with(root) {
+                    hbox {
+                        label("First Name") {
+                            hboxConstraints { margin = Insets(5.0) }
+                        }
+                        textfield {
+                            hboxConstraints { margin = Insets(5.0) }
+                            useMaxWidth = true
+                        }
+                    }
+                    hbox {
+                        label("Last Name") {
+                            hboxConstraints { margin = Insets(5.0) }
+                        }
+                        textfield {
+                            hboxConstraints { margin = Insets(5.0) }
+                            useMaxWidth = true
+                        }
+                    }
+                }
+            }
+        }
+
+        FxToolkit.setupFixture {
+            val root = Example1().root
+            primaryStage.scene = Scene(root)
+            primaryStage.show()
+        }
+
+        val robot = FxRobot()
+        robot.robotContext().captureSupport.saveImage(
+            robot.capture(primaryStage.scene.root), Paths.get("example-1.png"))
     }
 
 }
