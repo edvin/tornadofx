4b7f1da9b8562f637920a054624b415d37131c4b
Edvin Syse <es@syse.no>
2016-04-15 13:15:17 +0200

    ObservableStyleClass evaluates value when connected + added dispose function

--- src/main/java/tornadofx/CSS.kt
+++ src/main/java/tornadofx/CSS.kt
@@ -1,5 +1,6 @@
 package tornadofx
 
+import javafx.beans.value.ChangeListener
 import javafx.beans.value.ObservableValue
 import javafx.scene.Node
 
@@ -7,13 +8,24 @@ abstract class Stylesheet {
     abstract fun render(): String
 }
 
-class ObservableStyleClass(node: Node, value: ObservableValue<String>) {
+class ObservableStyleClass(node: Node, val value: ObservableValue<String>) {
+    val listener: ChangeListener<String>
+
     init {
-        value.addListener { observableValue, oldValue, newValue ->
-            if (oldValue != null) node.removeClass(oldValue)
+        fun checkAdd(newValue: String?) {
             if (newValue != null && !node.hasClass(newValue)) node.addClass(newValue)
         }
+
+        listener = ChangeListener { observableValue, oldValue, newValue ->
+            if (oldValue != null) node.removeClass(oldValue)
+            checkAdd(newValue)
+        }
+
+        checkAdd(value.value)
+        value.addListener(listener)
     }
+
+    fun dispose() = value.removeListener(listener)
 }
 
 fun Node.bindClass(value: ObservableValue<String>): ObservableStyleClass = ObservableStyleClass(this, value)
