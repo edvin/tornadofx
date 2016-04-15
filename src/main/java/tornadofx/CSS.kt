42c964520e81ac45c605f0f23d2d202aa2ff2742
Edvin Syse <es@syse.no>
2016-04-15 10:17:01 +0200

    ObservableStyleClass (https://github.com/edvin/tornadofx/issues/88)

--- src/main/java/tornadofx/CSS.kt
+++ src/main/java/tornadofx/CSS.kt
@@ -1,5 +1,19 @@
 package tornadofx
 
+import javafx.beans.value.ObservableValue
+import javafx.scene.Node
+
 abstract class Stylesheet {
     abstract fun render(): String
 }
+
+class ObservableStyleClass(node: Node, value: ObservableValue<String>) {
+    init {
+        value.addListener { observableValue, oldValue, newValue ->
+            if (oldValue != null) node.removeClass(oldValue)
+            if (newValue != null && !node.hasClass(newValue)) node.addClass(newValue)
+        }
+    }
+}
+
+fun Node.bindClass(value: ObservableValue<String>): ObservableStyleClass = ObservableStyleClass(this, value)
