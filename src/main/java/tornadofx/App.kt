8d9906881f3b49eb0738a28258dbf3dabe2eec6c
edvin <es@syse.no>
2016-05-05 17:35:37 +0200

    App can now start the primary view via named parameter `--view-class` for adhoc applications.

--- src/main/java/tornadofx/App.kt
+++ src/main/java/tornadofx/App.kt
@@ -2,19 +2,20 @@ package tornadofx
 
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.layout.Pane
 import javafx.stage.Stage
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 
-abstract class App : Application() {
-    abstract val primaryView: KClass<out View>
+open class App : Application() {
+    open val primaryView: KClass<out View> = DeterminedByParameter::class
 
     override fun start(stage: Stage) {
         FX.registerApplication(this, stage)
 
         try {
-            val view = find(primaryView)
+            val view = find(determinePrimaryView())
 
             stage.apply {
                 scene = Scene(view.root)
@@ -30,8 +31,23 @@ abstract class App : Application() {
         }
     }
 
+    @Suppress("UNCHECKED_CAST")
+    private fun determinePrimaryView(): KClass<out View> {
+        if (primaryView == DeterminedByParameter::class) {
+            val viewClassName = parameters.named?.get("view-class") ?: throw IllegalArgumentException("No provided --view-class parameter and primaryView was not overridden. Choose one strategy to specify the primary View")
+            val viewClass = Class.forName(viewClassName)
+            if (View::class.java.isAssignableFrom(viewClass)) return viewClass.kotlin as KClass<out View>
+            throw IllegalArgumentException("Class specified by --class-name is not a subclass of tornadofx.View")
+        } else {
+            return primaryView
+        }
+    }
+
     inline fun <reified T : Injectable> inject(): ReadOnlyProperty<App, T> = object : ReadOnlyProperty<App, T> {
         override fun getValue(thisRef: App, property: KProperty<*>) = find(T::class)
     }
 
+    class DeterminedByParameter : View() {
+        override val root = Pane()
+    }
 }
