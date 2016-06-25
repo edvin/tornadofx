0fd17ff7b8b4c7e0ba870f2f7ce912f3d48a1797
Thomas Nield <thomasnield@live.com>
2016-06-25 07:32:13 -0500

    Add Animation functions

---
+++ src/main/java/tornadofx/Animation.kt
@@ -0,0 +1,80 @@
+package tornadofx
+
+import javafx.animation.Interpolator
+import javafx.animation.KeyFrame
+import javafx.animation.KeyValue
+import javafx.animation.Timeline
+import javafx.beans.value.WritableValue
+import javafx.event.ActionEvent
+import javafx.scene.layout.Pane
+import javafx.util.Duration
+import java.util.ArrayList
+
+operator fun Timeline.plusAssign(keyFrame: KeyFrame) {
+    keyFrames.add(keyFrame)
+}
+
+operator fun KeyFrame.plusAssign(keyValue: KeyValue) {
+    values.add(keyValue)
+}
+
+fun Pane.timeline(op: (Timeline).() -> Unit): Timeline {
+    val timeline = Timeline()
+    timeline.op()
+    timeline.play()
+    return timeline
+}
+
+fun Timeline.keyframe(duration: Duration, op: (KeyFrameBuilder).() -> Unit): KeyFrame {
+    val keyFrameBuilder = KeyFrameBuilder(duration)
+    keyFrameBuilder.op()
+
+    return keyFrameBuilder.build().apply {
+        this@keyframe +=  this
+    }
+}
+
+class KeyFrameBuilder(val duration: Duration) {
+
+    var keyValues: MutableList<KeyValue> = ArrayList()
+    var name: String? = null
+    private var _onFinished: (ActionEvent) -> Unit = {}
+
+    fun setOnFinished(onFinished: (ActionEvent) -> Unit) {
+        this._onFinished = onFinished
+    }
+
+    operator fun plusAssign(keyValue: KeyValue) {
+        keyValues.add(keyValue)
+    }
+
+    fun <T> keyvalue(writableValue: WritableValue<T>, endValue: T, interpolator: Interpolator? = null): KeyValue {
+        val keyValue = interpolator?.let {  KeyValue(writableValue, endValue, it) } ?: KeyValue(writableValue, endValue)
+        this += keyValue
+        return keyValue
+    }
+
+    internal fun build() = KeyFrame(duration,name,_onFinished,keyValues)
+
+}
+
+fun <T> WritableValue<T>.animate(endValue: T, duration: Duration, interpolator: Interpolator? = null, op: (Timeline.() -> Unit)? = null) {
+    val writableValue = this
+    val timeline = Timeline()
+    op?.apply {this.invoke(timeline) }
+
+    timeline.apply {
+        keyframe(duration) {
+            keyvalue(writableValue,endValue,interpolator)
+        }
+        play()
+    }
+}
+
+val Number.millis: Duration get() = Duration.millis(this.toDouble())
+val Number.seconds: Duration get() = Duration.seconds(this.toDouble())
+val Number.minutes: Duration get() = Duration.minutes(this.toDouble())
+val Number.hours: Duration get() = Duration.hours(this.toDouble())
+
+operator fun Duration.plus(duration: Duration): Duration = this.add(duration)
+operator fun Duration.minus(duration: Duration): Duration = this.minus(duration)
