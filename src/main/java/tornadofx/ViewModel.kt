4ac0197112c04f74a7554ab9164f14cbd6c945b5
edvin <es@syse.no>
2016-07-07 08:15:41 +0200

    Tentative ViewModel while discussing PR 122

---
+++ src/main/java/tornadofx/ViewModel.kt
@@ -0,0 +1,38 @@
+package tornadofx
+
+import javafx.beans.property.ObjectProperty
+import javafx.beans.property.Property
+import javafx.beans.property.SimpleObjectProperty
+import javafx.collections.FXCollections
+import kotlin.reflect.KFunction
+import kotlin.reflect.KFunction2
+import kotlin.reflect.KMutableProperty1
+
+open class ViewModel {
+    private val properties = FXCollections.observableHashMap<Property<*>, Property<*>>()
+    private val dirtyProperties = FXCollections.observableHashMap<Property<*>, Property<*>>()
+
+    // Wrap Kotlin getter/setter property (var)
+    fun <S, T> S.wrap(prop: KMutableProperty1<S, T>): Property<T> =
+        observable(this, prop).wrap()
+
+    // Wrap POJO Property
+    fun <S : Any, T> S.wrap(getter: KFunction<T>, setter: KFunction2<S, T, Unit>): Property<T>
+            = observable(getter, setter).wrap()
+
+    // Wrap JavaFX Property
+    fun <S : Property<T>, T> S.wrap(): Property<T> {
+        val wrapper = SimpleObjectProperty<T>(this.value)
+        properties[wrapper] = this
+        return wrapper
+    }
+
+    fun commit() {
+        properties.forEach { it.value.value = it.key.value }
+    }
+
+    fun rollback() {
+        properties.forEach { it.key.value = it.value.value }
+    }
+
+}
---
+++ src/test/kotlin/tornadofx/ViewModelTests.kt
@@ -0,0 +1,52 @@
+package tornadofx
+
+import javafx.beans.property.Property
+import org.junit.Assert
+import org.junit.Test
+
+open class ViewModelTests {
+
+    @Test fun simple_commit() {
+        val person = Person("John")
+        val viewModel = PersonViewModel(person)
+
+        viewModel.name.value = "Jay"
+        Assert.assertEquals(person.name, "John")
+        viewModel.commit()
+        Assert.assertEquals(person.name, "Jay")
+    }
+
+    @Test fun pojo_commit() {
+        val person = JavaPerson()
+        person.name = "John"
+        val viewModel = JavaPersonViewModel(person)
+
+        viewModel.name.value = "Jay"
+        Assert.assertEquals(person.name, "John")
+        viewModel.commit()
+        Assert.assertEquals(person.name, "Jay")
+    }
+
+    @Test fun var_commit() {
+        val person = Person("John")
+        val viewModel = PersonVarViewModel(person)
+
+        viewModel.name.value = "Jay"
+        Assert.assertEquals(person.name, "John")
+        viewModel.commit()
+        Assert.assertEquals(person.name, "Jay")
+    }
+
+}
+
+class PersonViewModel(person: Person) : ViewModel() {
+    val name = person.nameProperty().wrap()
+}
+
+class JavaPersonViewModel(person: JavaPerson) : ViewModel() {
+    val name: Property<String> = person.wrap(JavaPerson::getName, JavaPerson::setName)
+}
+
+class PersonVarViewModel(person: Person) : ViewModel() {
+    val name = person.wrap(Person::name)
+}
