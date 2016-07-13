9a28bcafbec5b4f7771a535c473289013790954b
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2016-07-13 11:53:25 -0600

    Clarified return types

--- src/main/java/tornadofx/Properties.kt
+++ src/main/java/tornadofx/Properties.kt
@@ -177,27 +177,27 @@ operator fun <T> Property<T>.setValue(thisRef: Any?, property: KProperty<*>, val
     this.value = value
 }
 
-operator fun DoubleProperty.getValue(thisRef: Any?, property: KProperty<*>) = value!!
+operator fun DoubleProperty.getValue(thisRef: Any?, property: KProperty<*>): Double = value
 operator fun DoubleProperty.setValue(thisRef: Any?, property: KProperty<*>, value: Double) {
     this.value = value
 }
 
-operator fun FloatProperty.getValue(thisRef: Any?, property: KProperty<*>) = value!!
+operator fun FloatProperty.getValue(thisRef: Any?, property: KProperty<*>): Float = value
 operator fun FloatProperty.setValue(thisRef: Any?, property: KProperty<*>, value: Float) {
     this.value = value
 }
 
-operator fun LongProperty.getValue(thisRef: Any?, property: KProperty<*>) = value!!
+operator fun LongProperty.getValue(thisRef: Any?, property: KProperty<*>): Long = value
 operator fun LongProperty.setValue(thisRef: Any?, property: KProperty<*>, value: Long) {
     this.value = value
 }
 
-operator fun IntegerProperty.getValue(thisRef: Any?, property: KProperty<*>) = value!!
+operator fun IntegerProperty.getValue(thisRef: Any?, property: KProperty<*>): Int = value
 operator fun IntegerProperty.setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
     this.value = value
 }
 
-operator fun BooleanProperty.getValue(thisRef: Any?, property: KProperty<*>) = value!!
+operator fun BooleanProperty.getValue(thisRef: Any?, property: KProperty<*>): Boolean = value
 operator fun BooleanProperty.setValue(thisRef: Any?, property: KProperty<*>, value: Boolean) {
     this.value = value
 }
