bd93ce9af5f1d78d468e895d20b25c7f3d22ae46
edvin <es@syse.no>
2016-07-28 21:11:56 +0200

    Started OSGI project

---
+++ src/main/java/tornadofx/osgi/Activator.kt
@@ -0,0 +1,12 @@
+package tornadofx.osgi
+
+import org.osgi.framework.BundleActivator
+import org.osgi.framework.BundleContext
+
+class Activator : BundleActivator {
+    override fun start(context: BundleContext) {
+    }
+
+    override fun stop(context: BundleContext) {
+    }
+}
