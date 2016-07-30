d8b2d26b52055c9e8ea0038c9851e23c78e0f7a4
edvin <es@syse.no>
2016-07-30 14:21:00 +0200

    Added same mechanism for Stylesheet registration

--- src/main/java/tornadofx/osgi/StylesheetProvider.kt
+++ src/main/java/tornadofx/osgi/StylesheetProvider.kt
@@ -1,7 +1,17 @@
 package tornadofx.osgi
 
+import org.osgi.framework.BundleContext
+import tornadofx.Stylesheet
+import java.util.*
 import kotlin.reflect.KClass
 
 interface StylesheetProvider {
-    val stylesheet: KClass<out StylesheetProvider>
+    val stylesheet: KClass<out Stylesheet>
+}
+
+fun BundleContext.registerStylesheet(stylesheet: KClass<out Stylesheet>) {
+    val provider = object : StylesheetProvider {
+        override val stylesheet = stylesheet
+    }
+    registerService(StylesheetProvider::class.java, provider, Hashtable<String, String>())
 }
