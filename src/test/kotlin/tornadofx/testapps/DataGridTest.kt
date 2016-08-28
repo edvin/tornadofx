be7c705c4ecdd6983c7352cf7e768d250eadc615
edvin <es@syse.no>
2016-08-28 09:45:04 +0200

    Reorganized DataGridTest to better expose the need for more listeners as input changes

--- src/test/kotlin/tornadofx/testapps/DataGridTest.kt
+++ src/test/kotlin/tornadofx/testapps/DataGridTest.kt
@@ -1,28 +1,55 @@
 package tornadofx.testapps
 
 import tornadofx.*
+import tornadofx.testapps.DataGridTestApp.Companion.images
 
-class DataGridTestApp : App(DataGridTest::class, DataGridStyles::class)
+class DataGridTestApp : App(DataGridTest::class, DataGridStyles::class) {
+    companion object {
+        val images = mapOf(
+                "kittens" to listOf(
+                        "http://i.imgur.com/DuFZ6PQb.jpg",
+                        "http://i.imgur.com/o2QoeNnb.jpg",
+                        "http://i.imgur.com/P8wpBvub.jpg",
+                        "http://i.imgur.com/a2NDDglb.jpg",
+                        "http://i.imgur.com/79C37Scb.jpg",
+                        "http://i.imgur.com/N0fPCU2b.jpg",
+                        "http://i.imgur.com/qRfWC9wb.jpg",
+                        "http://i.imgur.com/i3crLYcb.jpg"),
+
+                "puppies" to listOf(
+                        "http://i.imgur.com/37hCL2Pb.jpg",
+                        "http://i.imgur.com/v9vBE67b.jpg",
+                        "http://i.imgur.com/koQoEExb.jpg",
+                        "http://i.imgur.com/ftDV8oJb.jpg")
+
+        )
+
+    }
+
+}
 
 class DataGridTest : View("DataGrid") {
-    val kittens = listOf(
-            "http://i.imgur.com/DuFZ6PQb.jpg",
-            "http://i.imgur.com/o2QoeNnb.jpg",
-            "http://i.imgur.com/P8wpBvub.jpg",
-            "http://i.imgur.com/a2NDDglb.jpg",
-            "http://i.imgur.com/79C37Scb.jpg",
-            "http://i.imgur.com/N0fPCU2b.jpg",
-            "http://i.imgur.com/qRfWC9wb.jpg",
-            "http://i.imgur.com/i3crLYcb.jpg")
+    var datagrid: DataGrid<String> by singleAssign()
 
-    override val root = datagrid(kittens) {
-        setPrefSize(530.0, 530.0)
+    override val root = borderpane {
+        left {
+            combobox(values = images.keys.toList()) {
+                valueProperty().onChange {
+                    datagrid.items.setAll(images[it])
+                }
+            }
+        }
+        center {
+            datagrid = datagrid<String> {
+                setPrefSize(530.0, 530.0)
 
-        cellWidth = 160.0
-        cellHeight = 160.0
+                cellWidth = 160.0
+                cellHeight = 160.0
 
-        cachedGraphic {
-            imageview(it, true)
+                cachedGraphic {
+                    imageview(it, true)
+                }
+            }
         }
     }
 
