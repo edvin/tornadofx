46600764163274370ed8ad0106003d5c71e54c1f
Carl Walker <carl@bekwam.com>
2016-10-22 09:06:24 -0400

    Brings back a simple listview example, repaired
    Uses new bindTo() expression in ListViewTest

--- src/test/kotlin/tornadofx/tests/SimpleListViewTest.kt
+++ src/test/kotlin/tornadofx/tests/SimpleListViewTest.kt
@@ -1,24 +1,15 @@
 package tornadofx.tests
 
-import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 
 class SimpleItem(var value: String)
 
-class MySimpleItemFragment : ListCellFragment<SimpleItem>() {
-    val itemValue = SimpleStringProperty()
-    override val root = hbox {
-        label(itemValue)
-    }
-}
-
 class SimpleListViewTestApp : App(SimpleListViewTest::class)
 
 class SimpleListViewTest : View("Simple ListCell Cache Test") {
-    val items = listOf(SimpleItem("One"), SimpleItem("Two"))
+    val items = listOf(SimpleItem("One"), SimpleItem("Two"), SimpleItem("Three"))
 
     override val root = listview(items.observable()) {
-        cellFragment(MySimpleItemFragment::class)
         cellFormat { text = it.value }
     }
 }
