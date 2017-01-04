2061edd1251712f36a88081dfeef4bfce92238eb
Edvin Syse <es@syse.no>
2017-01-04 10:53:47 +0100

    Adjusted slide padding sample app

--- src/test/kotlin/tornadofx/testapps/PaddingPropertyTest.kt
+++ src/test/kotlin/tornadofx/testapps/PaddingPropertyTest.kt
@@ -7,7 +7,7 @@ class PaddingPropertyTestApp : App(PaddingPropertyTest::class)
 
 class PaddingPropertyTest : View("Slide Padding") {
     override val root = vbox {
-        setPrefSize(400.0, 300.0)
+        setPrefSize(400.0, 150.0)
         style { backgroundColor += Color.CADETBLUE }
         slider(0.0, 100.0, 20.0) {
             this@vbox.paddingAllProperty.bind(valueProperty())
