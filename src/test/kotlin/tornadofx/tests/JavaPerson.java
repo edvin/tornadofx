86850c455221488fb30304580cf7b3756e127897
gtnarg <gtnarg@users.noreply.github.com>
2017-01-22 12:29:56 -0500

    - can now bind to a pojo by providing only a single getter ( eg. person.observable( JavaPerson::getId ) )   - API break: previously returned a PojoProperty - now returns an ObjectProperty<T>   - uses javafx.beans.property.adapter.JavaBeanObjectPropertyBuilder and will now propogate PropertyChangeEvents from the pojo

--- src/test/kotlin/tornadofx/tests/JavaPerson.java
+++ src/test/kotlin/tornadofx/tests/JavaPerson.java
@@ -1,15 +1,26 @@
 package tornadofx.tests;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class JavaPerson {
+
+    private PropertyChangeSupport pcs;
     private Integer id;
     private String name;
+
+    public JavaPerson(){
+        pcs = new PropertyChangeSupport(this);
+    }
 
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
+        Integer oldValue = this.id;
         this.id = id;
+        //pcs.firePropertyChange("id",oldValue,id);
     }
 
     public String getName() {
@@ -17,7 +28,9 @@ public class JavaPerson {
     }
 
     public void setName(String name) {
+        String oldValue = this.name;
         this.name = name;
+        pcs.firePropertyChange("name",oldValue,name);
     }
 
     public String toString() {
@@ -26,4 +39,14 @@ public class JavaPerson {
                 ", name='" + name + '\'' +
                 '}';
     }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.removePropertyChangeListener(listener);
+    }
+
+
 }
