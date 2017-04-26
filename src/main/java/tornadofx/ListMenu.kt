29f486a93d63f2132eb0afc5acc1e15706f9f625
Edvin Syse <es@syse.no>
2017-04-26 23:13:17 +0200

    ListMenu enhancements.

--- src/main/java/tornadofx/ListMenu.kt
+++ src/main/java/tornadofx/ListMenu.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused", "UNCHECKED_CAST")
+
 package tornadofx
 
 import javafx.beans.DefaultProperty
@@ -15,14 +17,16 @@ import javafx.scene.Node
 import javafx.scene.control.Control
 import javafx.scene.control.Skin
 import javafx.scene.control.SkinBase
+import javafx.scene.image.ImageView
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.Region
 import javafx.scene.text.Text
 
 @DefaultProperty("children")
 class ListMenu : Control() {
-    private val graphicFixedSize = FACTORY.createStyleableNumberProperty(this, "graphicFixedSize", "-fx-graphic-fixed-size", { it.graphicFixedSizeProperty })
-    val graphicFixedSizeProperty: StyleableProperty<Number> get() = graphicFixedSize
+    private val graphicFixedSizeInternal = FACTORY.createStyleableNumberProperty(this, "graphicFixedSize", "-fx-graphic-fixed-size", { it.graphicFixedSizeProperty })
+    val graphicFixedSizeProperty: StyleableProperty<Number> get() = graphicFixedSizeInternal
+    var graphicFixedSized: Number get() = graphicFixedSizeInternal.value; set(value) { graphicFixedSizeInternal.value = value }
 
     val themeProperty = SimpleStringProperty()
     var theme by themeProperty
@@ -48,8 +52,6 @@ class ListMenu : Control() {
     }
     var orientation: Orientation by orientationProperty
 
-    val items: List<ListMenuItem> get() = children.map { it as? ListMenuItem }.filterNotNull()
-
     val iconPositionProperty: ObjectProperty<Side> = object : SimpleObjectProperty<Side>(Side.LEFT) {
         override fun invalidated() {
             children.forEach { child ->
@@ -72,8 +74,7 @@ class ListMenu : Control() {
 
     override fun getControlCssMetaData(): List<CssMetaData<out Styleable, *>> = FACTORY.cssMetaData
     override fun createDefaultSkin() = ListMenuSkin(this)
-    public override fun getChildren(): ObservableList<Node> = super.getChildren()
-
+    val items: ObservableList<ListMenuItem> get() = children as ObservableList<ListMenuItem>
     override fun getUserAgentStylesheet(): String = ListMenu::class.java.getResource("listmenu.css").toExternalForm()
 
     companion object {
@@ -114,25 +115,25 @@ class ListMenuSkin(control: ListMenu) : SkinBase<ListMenu>(control) {
     private fun biggest(fn: (Node) -> Double) = children.map { fn(it) }.max() ?: 0.0
 
     override fun computeMinWidth(height: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double) =
-            if (skinnable.orientation == VERTICAL) biggest { n -> n.minWidth(height) } else acc { n -> n.minWidth(height) }
+            if (skinnable.orientation == VERTICAL) biggest { it.minWidth(height) } else acc { it.minWidth(height) }
 
     override fun computeMinHeight(width: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double) =
-            if (skinnable.orientation == VERTICAL) acc { n -> n.minHeight(width) } else biggest { n -> n.minHeight(width) }
+            if (skinnable.orientation == VERTICAL) acc { it.minHeight(width) } else biggest { it.minHeight(width) }
 
     override fun computePrefWidth(height: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double): Double {
         val prefWidth = if (skinnable.orientation == VERTICAL)
-            biggest { n -> n.prefWidth(height) } + leftInset + rightInset
+            biggest { it.prefWidth(height) } + leftInset + rightInset
         else
-            acc { n -> n.prefWidth(height) } + leftInset + rightInset
+            acc { it.prefWidth(height) } + leftInset + rightInset
 
         return Math.max(prefWidth, skinnable.prefWidth)
     }
 
     override fun computePrefHeight(width: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double): Double {
         val prefHeight = if (skinnable.orientation == VERTICAL)
-            acc { n -> n.prefHeight(width) } + topInset + bottomInset
+            acc { it.prefHeight(width) } + topInset + bottomInset
         else
-            biggest { n -> n.prefHeight(width) } + topInset + bottomInset
+            biggest { it.prefHeight(width) } + topInset + bottomInset
 
         return Math.max(prefHeight, skinnable.prefHeight)
     }
@@ -144,36 +145,40 @@ class ListMenuSkin(control: ListMenu) : SkinBase<ListMenu>(control) {
             computePrefHeight(width, topInset, rightInset, bottomInset, leftInset)
 
     override fun layoutChildren(x: Double, y: Double, w: Double, h: Double) {
-        var xx = x
-        var yy = y
+        var currentX = x
+        var currentY = y
         for (node in children) {
             if (skinnable.orientation == VERTICAL) {
                 val prefHeight = node.prefHeight(-1.0)
-                node.resizeRelocate(xx, yy, w, prefHeight)
-                yy += prefHeight
+                node.resizeRelocate(currentX, currentY, w, prefHeight)
+                currentY += prefHeight
             } else {
                 val prefWidth = node.prefWidth(-1.0)
-                node.resizeRelocate(xx, yy, prefWidth, h)
-                xx += prefWidth
+                node.resizeRelocate(currentX, currentY, prefWidth, h)
+                currentX += prefWidth
             }
         }
     }
-
 }
 
 class ListMenuItemSkin(control: ListMenuItem) : SkinBase<ListMenuItem>(control) {
     val text = Text()
 
+    private fun registerGraphic(node: Node) {
+        children.add(node)
+        node.addClass("graphic")
+        (node as? ImageView)?.isMouseTransparent = true
+    }
+
     init {
         text.textProperty().bind(control.textProperty)
         children.add(text)
 
-        if (skinnable.graphic != null)
-            children.add(skinnable.graphic)
+        if (skinnable.graphic != null) registerGraphic(skinnable.graphic!! )
 
         skinnable.graphicProperty.addListener { _, old, new ->
             if (old != null) children.remove(old)
-            if (new != null) children.add(new)
+            if (new != null) registerGraphic(new)
         }
 
         control.addEventHandler(MouseEvent.MOUSE_PRESSED) {
@@ -182,53 +187,28 @@ class ListMenuItemSkin(control: ListMenuItem) : SkinBase<ListMenuItem>(control) 
         }
     }
 
-    private fun acc(fn: (Node) -> Double): Double {
-        var v = fn(text)
-        if (skinnable.graphic != null)
-            v += fn(skinnable.graphic!!)
-        return v
-    }
-
-    private fun biggest(fn: (Node) -> Double): Double {
-        val v = fn(text)
+    override fun computePrefWidth(height: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double): Double {
+        var w = if (text.text.isNullOrBlank()) 0.0 else text.prefWidth(height)
 
         if (skinnable.graphic != null) {
-            val gval = fn(skinnable.graphic!!)
-            if (gval > v) return gval
+            if (iconPosition.isVertical)
+                w += Math.max(skinnable.graphic!!.prefWidth(-1.0), graphicFixedSize)
+            else
+                w = Math.max(w, Math.max(skinnable.graphic!!.prefWidth(-1.0), graphicFixedSize))
         }
-
-        return v
-    }
-
-    override fun computeMinWidth(height: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double): Double {
-        if (iconPosition.isHorizontal)
-            return biggest({ n -> n.minWidth(height) })
-        else
-            return acc({ n -> n.minWidth(height) })
-    }
-
-    override fun computeMinHeight(width: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double): Double {
-        if (iconPosition.isHorizontal)
-            return acc({ n -> n.minHeight(width) })
-        else
-            return biggest({ n -> n.minHeight(width) })
-    }
-
-    override fun computePrefWidth(height: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double): Double {
-        var w = text.prefWidth(height)
-
-        if (skinnable.graphic != null && iconPosition.isVertical)
-            w += Math.max(skinnable.graphic!!.prefWidth(-1.0), graphicFixedSize)
 
         return w + leftInset + rightInset
     }
 
     override fun computePrefHeight(width: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double): Double {
-        var h = text.prefHeight(width)
+        var h = if (text.text.isNullOrBlank()) 0.0 else text.prefHeight(width)
 
-        if (skinnable.graphic != null && iconPosition.isHorizontal)
-            h += Math.max(skinnable.graphic!!.prefHeight(-1.0), graphicFixedSize)
-
+        if (skinnable.graphic != null) {
+            if (iconPosition.isHorizontal)
+                h += Math.max(skinnable.graphic!!.prefHeight(-1.0), graphicFixedSize)
+            else
+                h = Math.max(h, Math.max(skinnable.graphic!!.prefHeight(-1.0), graphicFixedSize))
+        }
         return h + topInset + bottomInset
     }
 
@@ -240,66 +220,82 @@ class ListMenuItemSkin(control: ListMenuItem) : SkinBase<ListMenuItem>(control) 
         return computePrefHeight(width, topInset, rightInset, bottomInset, leftInset)
     }
 
+    override fun computeMinWidth(height: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double) =
+            computePrefWidth(height, topInset, rightInset, bottomInset, leftInset)
+
+    override fun computeMinHeight(width: Double, topInset: Double, rightInset: Double, bottomInset: Double, leftInset: Double) =
+            computePrefHeight(width, topInset, rightInset, bottomInset, leftInset)
+
     private val iconPosition: Side get() = skinnable.menu.iconPosition
 
+    private val textPrefWidth: Double = if (text.text.isNullOrBlank()) 0.0 else text.prefWidth(-1.0)
+    private val textPrefHeight: Double = if (text.text.isNullOrBlank()) 0.0 else text.prefHeight(-1.0)
+
     override fun layoutChildren(x: Double, y: Double, w: Double, h: Double) {
-        var xx = x
-        var yy = y
+        var currentX = x
+        var currentY = y
         val graphic = skinnable.graphic
+        val hasText = textPrefWidth > 0.0
+
+        if (!hasText) {
+            if (graphic != null) {
+                val centeredX = x + w / 2 - graphic.layoutBounds.width / 2
+                val centeredY = y + h / 2 - graphic.layoutBounds.height / 2
+                graphic.relocate(centeredX, centeredY)
+            }
+            return
+        }
 
         when (iconPosition) {
             Side.TOP -> {
                 if (graphic != null) {
-                    val centeredX = xx + w / 2 - graphic.layoutBounds.width / 2
-                    graphic.relocate(centeredX, yy)
-                    yy += Math.max(graphic.layoutBounds.height, graphicFixedSize)
+                    val centeredX = currentX + w / 2 - graphic.layoutBounds.width / 2
+                    graphic.relocate(centeredX, currentY)
+                    currentY += Math.max(graphic.layoutBounds.height, graphicFixedSize)
                 }
-
-                val centeredX = xx + w / 2 - text.prefWidth(-1.0) / 2
-                text.resizeRelocate(centeredX, yy, text.prefWidth(-1.0), text.prefHeight(-1.0))
+                val centeredX = currentX + w / 2 - textPrefWidth / 2
+                text.resizeRelocate(centeredX, currentY, textPrefWidth, textPrefHeight)
             }
             Side.BOTTOM -> {
-                var centeredX = xx + w / 2 - text.prefWidth(-1.0) / 2
-                text.resizeRelocate(centeredX, yy, text.prefWidth(-1.0), text.prefHeight(-1.0))
+                if (hasText) {
+                    val centeredX = currentX + w / 2 - textPrefWidth / 2
+                    text.resizeRelocate(centeredX, currentY, textPrefWidth, textPrefHeight)
+                    currentY += textPrefHeight
+                }
 
                 if (graphic != null) {
-                    yy += text.prefHeight(-1.0)
                     val fixedSize = graphicFixedSize
-                    if (fixedSize > graphic.layoutBounds.height)
-                        yy += fixedSize - graphic.layoutBounds.height
-
-                    centeredX = xx + w / 2 - graphic.layoutBounds.width / 2
-                    graphic.relocate(centeredX, yy)
+                    if (fixedSize > graphic.layoutBounds.height) {
+                        currentY += (fixedSize - graphic.layoutBounds.height) / 2
+                    }
+                    val centeredX = currentX + w / 2 - graphic.layoutBounds.width / 2
+                    graphic.relocate(centeredX, currentY)
                 }
             }
 
             Side.LEFT -> {
                 if (graphic != null) {
-                    val centeredY = yy + h / 2 - graphic.layoutBounds.height / 2
-                    graphic.relocate(xx, centeredY)
-
-                    xx += Math.max(graphic.layoutBounds.width, graphicFixedSize)
+                    val centeredY = currentY + h / 2 - graphic.layoutBounds.height / 2
+                    graphic.relocate(currentX, centeredY)
+                    currentX += Math.max(graphic.layoutBounds.width, graphicFixedSize)
                 }
-
-                val centeredY = yy + h / 2 - text.prefHeight(-1.0) / 2
-                text.resizeRelocate(xx, centeredY, text.prefWidth(-1.0), text.prefHeight(-1.0))
+                val centeredY = currentY + h / 2 - textPrefHeight / 2
+                text.resizeRelocate(currentX, centeredY, textPrefWidth, textPrefHeight)
             }
             Side.RIGHT -> {
                 if (graphic != null) {
-                    val centeredY = yy + h / 2 - graphic.layoutBounds.height / 2
+                    val centeredY = currentY + h / 2 - graphic.layoutBounds.height / 2
                     val graphicWidth = Math.max(graphic.layoutBounds.width, graphicFixedSize)
-
-                    graphic.resizeRelocate(w, centeredY, graphicWidth, graphic.prefHeight(-1.0))
+                    graphic.resizeRelocate(w - skinnable.padding.right, centeredY, graphicWidth, graphic.prefHeight(-1.0))
                 }
 
-                val centeredY = yy + h / 2 - text.prefHeight(-1.0) / 2
-                text.resizeRelocate(xx, centeredY, text.prefWidth(-1.0), text.prefHeight(-1.0))
+                val centeredY = currentY + h / 2 - textPrefHeight / 2
+                text.resizeRelocate(currentX, centeredY, textPrefWidth, textPrefHeight)
             }
         }
     }
 
     private val graphicFixedSize: Double get() = skinnable.menu.graphicFixedSizeProperty.value.toDouble()
-
 }
 
 fun EventTarget.listmenu(orientation: Orientation = VERTICAL, iconPosition: Side = Side.LEFT, theme: String? = null, op: (ListMenu.() -> Unit)? = null): ListMenu {
