a5dff6612777350b582e060155b54dc0e7056a19
Edvin Syse <es@syse.no>
2017-09-10 14:52:04 +0200

    Pass correct column type to TornadoFXTable.doResize

--- src/main/java/tornadofx/adapters/TornadoFXColumns.kt
+++ src/main/java/tornadofx/adapters/TornadoFXColumns.kt
@@ -8,7 +8,7 @@ fun TreeTableColumn<*,*>.toTornadoFXColumn() = TornadoFXTreeTableColumn(this)
 fun TableColumn<*,*>.toTornadoFXColumn() = TornadoFxNormalTableColumn(this)
 
 interface TornadoFXColumn<COLUMN> {
-    val column: Any
+    val column: COLUMN
     val properties: Properties
     var prefWidth: Double
     var maxWidth: Double
@@ -46,23 +46,23 @@ class TornadoFXTreeTableColumn(override val column: TreeTableColumn<*, *>) : Tor
 
 class TornadoFxNormalTableColumn(override val column: TableColumn<*, *>) : TornadoFXColumn<TableColumn<*, *>> {
     override var minWidth: Double
-        get() = column.minWidth;
+        get() = column.minWidth
         set(value) {
             column.minWidth = value
         }
     override var maxWidth: Double
-        get() = column.maxWidth;
+        get() = column.maxWidth
         set(value) {
             column.maxWidth = value
         }
     override var prefWidth: Double
-        get() = column.prefWidth;
+        get() = column.prefWidth
         set(value) {
             column.prefWidth = value
         }
     override val properties = column.properties
     override var width: Double
-        get() = column.width;
+        get() = column.width
         set(value) {
             column.prefWidth = value
         }
