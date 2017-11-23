ce035c5fa9f025ee157c7005bc89eedf1ffeba1b
Leonid <sleonidy@gmail.com>
2017-11-23 20:41:24 +0200

    Moved documentation to interface and made property internal

--- src/main/java/tornadofx/FX.kt
+++ src/main/java/tornadofx/FX.kt
@@ -130,21 +130,7 @@ class FX {
 
         val lock = Any()
 
-        /**
-         * An interceptor that can veto or provide another mechanism for adding children to their parent.
-         *
-         * This callback is called for all builders right before the default mechanism adds the newly created
-         * node to the parent container. Returning false means that the default will be used. Returning true means
-         * that you added the child yourself and that the default mechanism should do nothing.
-         *
-         * This is useful for layout containers that need special handling when adding child nodes.
-         *
-         * Example: MigPane needs to add a default ComponentConstraint object to be able to manipulate the
-         * constraint after the child is added.
-         *
-         * FX.addChildInterceptor = { parent, node, index -> if (parent is MigPane) { parent.add(node, CC()); true } else false }
-         */
-        val childInterceptors = mutableListOf<ChildInterceptor>()
+        internal val childInterceptors = mutableListOf<ChildInterceptor>()
 
         @JvmStatic
         var dicontainer: DIContainer? = null
