e50b264ab70850c5897ab222cc08717bf3265799
Kirk Stork <kastork@nps.edu>
2017-12-01 14:44:41 -0800

    Eliminate interactions between tests
    On some systems, the order of test execution could impact tests passing or failing, indicating that state from one test could be carried over to other tests.
    
    This commit does two things to try and avoid that situation.
    
    1. Sets up the Maven Surefire plugin to run classes in separate forks (JVMs).
    
    2. Refactor the two problematic tests to use the same approach for setting up and tearing down the FX environment they run in.
    
    On my macOS system all tests now pass when running from Maven and also when running directly in IDEA.

--- src/test/kotlin/tornadofx/tests/StylesheetErrorTest.kt
+++ src/test/kotlin/tornadofx/tests/StylesheetErrorTest.kt
@@ -1,24 +1,44 @@
 package tornadofx.tests
 
 import javafx.stage.Stage
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Test
 import org.testfx.api.FxAssert.verifyThat
-import org.testfx.framework.junit.ApplicationTest
+import org.testfx.api.FxToolkit
 import org.testfx.matcher.control.LabeledMatchers.hasText
-import tornadofx.testapps.StylesheetErrorTest
+import tornadofx.*
+import tornadofx.testapps.Styles
+import tornadofx.testapps.StylesheetErrorView
 
-class StylesheetErrorTest : ApplicationTest() {
+//@Ignore
+class StylesheetErrorTest {
 
-    /**
-     * This will cause a stylesheet error, but should not crash the application.
-     */
-    override fun start(stage: Stage) {
-        StylesheetErrorTest().start(stage)
-    }
+	companion object {
 
-    @Test
-    fun shouldStartApplicationWithWrongStylesheetWithoutCrashing() {
-        verifyThat(".my-button", hasText("Click here"))
-    }
+		val app = App(StylesheetErrorView::class, Styles::class)
+		@JvmStatic
+		@BeforeClass
+		fun before() {
+			val primaryStage: Stage = FxToolkit.registerPrimaryStage()
+			/**
+			 * This will cause a stylesheet error, but should not crash the application.
+			 */
+			FX.registerApplication(FxToolkit.setupApplication {
+				app
+			}, primaryStage)
+		}
+
+		@JvmStatic
+		@AfterClass
+		fun after() {
+			FxToolkit.cleanupApplication(app)
+		}
+	}
+
+	@Test
+	fun shouldStartApplicationWithWrongStylesheetWithoutCrashing() {
+		verifyThat(".my-button", hasText("Click here"))
+	}
 
 }
