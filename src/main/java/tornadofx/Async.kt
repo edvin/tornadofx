ec6188d01d6df5e9788ada7e357c182405aa21e4
jansorg <github@joachim-ansorg.de>
2018-02-05 10:56:58 +0100

    Use public Platform methods because the Application isn't accessible in Java 9 anymore.

--- src/main/java/tornadofx/Async.kt
+++ src/main/java/tornadofx/Async.kt
@@ -1,7 +1,5 @@
 package tornadofx
 
-import com.sun.glass.ui.Application
-import com.sun.javafx.tk.Toolkit
 import javafx.application.Platform
 import javafx.beans.property.*
 import javafx.beans.value.ChangeListener
@@ -111,7 +109,7 @@ infix fun <T> Task<T>.success(func: (T) -> Unit) = apply {
         }
     }
 
-    if (Application.isEventThread())
+    if (Platform.isFxApplicationThread())
         attachSuccessHandler()
     else
         runLater { attachSuccessHandler() }
@@ -128,7 +126,7 @@ infix fun <T> Task<T>.fail(func: (Throwable) -> Unit) = apply {
         }
     }
 
-    if (Application.isEventThread())
+    if (Platform.isFxApplicationThread())
         attachFailHandler()
     else
         runLater { attachFailHandler() }
@@ -164,15 +162,11 @@ fun runLater(delay: Duration, op: () -> Unit): FXTimerTask {
  * This method does not block the UI thread even though it halts further execution until the condition is met.
  */
 fun <T> ObservableValue<T>.awaitUntil(condition: (T) -> Boolean) {
-    if (!Toolkit.getToolkit().canStartNestedEventLoop()) {
-        throw IllegalStateException("awaitUntil is not allowed during animation or layout processing")
-    }
-
     val changeListener = object : ChangeListener<T> {
         override fun changed(observable: ObservableValue<out T>?, oldValue: T, newValue: T) {
             if (condition(value)) {
                 runLater {
-                    Toolkit.getToolkit().exitNestedEventLoop(this@awaitUntil, null)
+                    Platform.exitNestedEventLoop(this@awaitUntil, null)
                     removeListener(this)
                 }
             }
@@ -181,7 +175,7 @@ fun <T> ObservableValue<T>.awaitUntil(condition: (T) -> Boolean) {
 
     changeListener.changed(this, value, value)
     addListener(changeListener)
-    Toolkit.getToolkit().enterNestedEventLoop(this)
+    Platform.enterNestedEventLoop(this)
 }
 
 /**
