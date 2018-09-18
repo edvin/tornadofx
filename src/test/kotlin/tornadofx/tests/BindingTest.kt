b9770d22409a5d05d96e5626d1ab1f76b2ba0923
Juan José González Abril <SackCastellon@users.noreply.github.com>
2018-09-18 23:45:51 +0200

    Fixed formatting, simplified assertion on list and removed unused code.

--- src/test/kotlin/tornadofx/tests/BindingTest.kt
+++ src/test/kotlin/tornadofx/tests/BindingTest.kt
@@ -5,52 +5,61 @@ import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleDoubleProperty
 import javafx.collections.FXCollections
 import javafx.scene.control.Label
-import javafx.stage.Stage
+import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.testfx.api.FxToolkit
 import tornadofx.*
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class BindingTest {
-    val primaryStage: Stage = FxToolkit.registerPrimaryStage()
+
+    @Before
+    fun setupFX() {
+        FxToolkit.registerPrimaryStage()
+    }
 
     @Test
     fun testBindingFormat() {
         val property = SimpleDoubleProperty(Math.PI)
-
         val label = Label()
         label.bind(property, format = DecimalFormat("#0.0", DecimalFormatSymbols.getInstance(Locale.ENGLISH)))
 
-        Assert.assertEquals("3.1", label.text)
+        assertEquals("3.1", label.text)
     }
 
     @Test
     fun observableListBinding() {
         val elements = FXCollections.observableArrayList("Hello", "World")
-        val binding = stringBinding(elements, elements) { this.joinToString(" ")}
-        val uielement = Label().apply { bind(binding) }
-        Assert.assertEquals("Hello World", uielement.text)
+        val binding = stringBinding(elements, elements) { this.joinToString(" ") }
+        val uiElement = Label().apply { bind(binding) }
+
+        assertEquals("Hello World", uiElement.text)
+
         elements.setAll("Hello", "Changed")
-        Assert.assertEquals("Hello Changed", uielement.text)
+        assertEquals("Hello Changed", uiElement.text)
     }
 
     @Test
     fun observableMapBinding() {
         val map = FXCollections.observableHashMap<String, Int>()
         val list = observableList<Char>()
+        list.bind(map) { k, v -> k[v] }
+
         map["0123456"] = 3
-        list.bind(map){k,v-> k[v]}
-        Assert.assertEquals(list, listOf('3'))
+        assertEquals(list, listOf('3'))
+
         map["0123456"] = 4
-        Assert.assertEquals(list, listOf('4'))
+        assertEquals(list, listOf('4'))
+
         map["abcdefg"] = 3
-        Assert.assertTrue(
-                "expected ['d', '4'] got ${list.joinToString() }}",
-                list.containsAll(listOf('d', '4')) && list.size == 2
-        )
+        Assert.assertThat(list, containsInAnyOrder('d', '4'))
     }
 
     @Test
@@ -58,33 +67,40 @@ class BindingTest {
         val father = Person("Mr Father", 50)
         val stepFather = Person("Mr Step Father", 40)
         val child = Person("Firstborn Child", 18)
-        child.parent = father
-
         val fatherName = child.parentProperty().select(Person::nameProperty)
-        Assert.assertEquals("Mr Father", fatherName.value)
+
+        child.parent = father
+        assertEquals("Mr Father", fatherName.value)
+
         fatherName.value = "Mister Father"
-        Assert.assertEquals("Mister Father", father.name)
+        assertEquals("Mister Father", father.name)
+
         child.parent = stepFather
-        Assert.assertEquals("Mr Step Father", fatherName.value)
+        assertEquals("Mr Step Father", fatherName.value)
     }
 
     @Test
     fun booleanBinding() {
-        val mylist = FXCollections.observableArrayList<String>()
-        val complete = booleanBinding(mylist) { isNotEmpty() }
-        Assert.assertFalse(complete.value)
-        mylist.add("One")
-        Assert.assertTrue(complete.value)
+        val myList = FXCollections.observableArrayList<String>()
+        val complete = booleanBinding(myList) { isNotEmpty() }
+
+        assertFalse(complete.value)
+
+        myList.add("One")
+        assertTrue(complete.value)
     }
 
     @Test
     fun booleanListBinding() {
-        val mylist = FXCollections.observableArrayList<BooleanExpression>()
-        val complete = booleanListBinding(mylist) { this }
-        Assert.assertFalse(complete.value)
-        mylist.add(SimpleBooleanProperty(true))
-        Assert.assertTrue(complete.value)
-        mylist.add(SimpleBooleanProperty(false))
-        Assert.assertFalse(complete.value)
+        val myList = FXCollections.observableArrayList<BooleanExpression>()
+        val complete = booleanListBinding(myList) { this }
+
+        assertFalse(complete.value)
+
+        myList.add(SimpleBooleanProperty(true))
+        assertTrue(complete.value)
+
+        myList.add(SimpleBooleanProperty(false))
+        assertFalse(complete.value)
     }
 }
