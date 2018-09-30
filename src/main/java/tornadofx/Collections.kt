2cf487f5f0b5a0f6833f4c780136d8d9934d1d67
Juan José González Abril <SackCastellon@users.noreply.github.com>
2018-10-01 00:49:26 +0200

    Refactorization and cleanup

--- src/main/java/tornadofx/Collections.kt
+++ src/main/java/tornadofx/Collections.kt
@@ -1,5 +1,3 @@
-@file:Suppress("unused")
-
 package tornadofx
 
 import javafx.beans.WeakListener
@@ -9,33 +7,27 @@ import tornadofx.FX.IgnoreParentBuilder.Once
 import java.lang.ref.WeakReference
 import java.util.*
 
-/**
- * Moves the given **T** item to the specified index
- */
+/** Moves the given [item] to the specified [newIndex]. */
 fun <T> MutableList<T>.move(item: T, newIndex: Int) {
-    check(newIndex in 0 until size)
+    check(newIndex in indices) { "Invalid index $newIndex for MutableList of size $size" }
     val currentIndex = indexOf(item)
     if (currentIndex < 0) return
     removeAt(currentIndex)
     add(newIndex, item)
 }
 
-/**
- * Moves the given item at the `oldIndex` to the `newIndex`
- */
+/** Moves the given item at the [oldIndex] to the [newIndex]. */
 fun <T> MutableList<T>.moveAt(oldIndex: Int, newIndex: Int) {
-    check(oldIndex in 0 until size)
-    check(newIndex in 0 until size)
+    check(oldIndex in indices) { "Invalid index $oldIndex for MutableList of size $size" }
+    check(newIndex in indices) { "Invalid index $newIndex for MutableList of size $size" }
     val item = this[oldIndex]
     removeAt(oldIndex)
     add(newIndex, item)
 }
 
-/**
- * Moves all items meeting a predicate to the given index
- */
+/** Moves all items meeting a [predicate] to the given [newIndex]. */
 fun <T> MutableList<T>.moveAll(newIndex: Int, predicate: (T) -> Boolean) {
-    check(newIndex in 0 until size)
+    check(newIndex in indices) { "Invalid index $newIndex for MutableList of size $size" }
     val split = partition(predicate)
     clear()
     addAll(split.second)
@@ -43,35 +35,28 @@ fun <T> MutableList<T>.moveAll(newIndex: Int, predicate: (T) -> Boolean) {
 }
 
 /**
- * Moves the given element at specified index up the **MutableList** by one increment
- * unless it is at the top already which will result in no movement
+ * Moves the given element at specified [index] up by one increment
+ * unless it is at the top already which will result in no movement.
  */
 fun <T> MutableList<T>.moveUpAt(index: Int) {
     if (index == 0) return
-    check(index in indices, { "Invalid index $index for MutableList of size $size" })
-    val newIndex = index - 1
-    val item = this[index]
-    removeAt(index)
-    add(newIndex, item)
+    check(index in indices) { "Invalid index $index for MutableList of size $size" }
+    swap(index, index - 1)
 }
 
 /**
- * Moves the given element **T** up the **MutableList** by one increment
- * unless it is at the bottom already which will result in no movement
+ * Moves the given element at specified [index] down by one increment
+ * unless it is at the bottom already which will result in no movement.
  */
 fun <T> MutableList<T>.moveDownAt(index: Int) {
     if (index == size - 1) return
-    check(index in indices, { "Invalid index $index for MutableList of size $size" })
-    val newIndex = index + 1
-    val item = this[index]
-    removeAt(index)
-    add(newIndex, item)
+    check(index in indices) { "Invalid index $index for MutableList of size $size" }
+    swap(index, index + 1)
 }
 
 /**
- * Moves the given element **T** up the **MutableList** by an index increment
- * unless it is at the top already which will result in no movement.
- * Returns a `Boolean` indicating if move was successful
+ * Moves the given [item] up by an index increment unless it is at the top already which will result in no movement.
+ * @return `true` if move was successful, `false` otherwise.
  */
 fun <T> MutableList<T>.moveUp(item: T): Boolean {
     val currentIndex = indexOf(item)
@@ -84,9 +69,8 @@ fun <T> MutableList<T>.moveUp(item: T): Boolean {
 }
 
 /**
- * Moves the given element **T** up the **MutableList** by an index increment
- * unless it is at the bottom already which will result in no movement.
- * Returns a `Boolean` indicating if move was successful
+ * Moves the given [item] down by an index increment unless it is at the bottom already which will result in no movement.
+ * @return `true` if move was successful, `false` otherwise.
  */
 fun <T> MutableList<T>.moveDown(item: T): Boolean {
     val currentIndex = indexOf(item)
@@ -100,63 +84,54 @@ fun <T> MutableList<T>.moveDown(item: T): Boolean {
 
 
 /**
- * Moves first element **T** up an index that satisfies the given **predicate**, unless its already at the top
+ * Moves the first element that satisfies the given [predicate] up an index, unless its already at the top.
+ * @return `true` if move was successful, `false` otherwise or `null` if no element matched the [predicate].
  */
-inline fun <T> MutableList<T>.moveUp(crossinline predicate: (T) -> Boolean) = find(predicate)?.let { moveUp(it) }
+inline fun <T> MutableList<T>.moveUp(crossinline predicate: (T) -> Boolean): Boolean? = find(predicate)?.let { moveUp(it) }
 
 /**
- * Moves first element **T** down an index that satisfies the given **predicate**, unless its already at the bottom
+ * Moves the first element that satisfies the given [predicate] down an index, unless its already at the bottom.
+ * @return `true` if move was successful, `false` otherwise or `null` if no element matched the [predicate].
  */
-inline fun <T> MutableList<T>.moveDown(crossinline predicate: (T) -> Boolean) = find(predicate)?.let { moveDown(it) }
+inline fun <T> MutableList<T>.moveDown(crossinline predicate: (T) -> Boolean): Boolean? = find(predicate)?.let { moveDown(it) }
 
-/**
- * Moves all **T** elements up an index that satisfy the given **predicate**, unless they are already at the top
- */
-inline fun <T> MutableList<T>.moveUpAll(crossinline predicate: (T) -> Boolean) = asSequence().withIndex()
-        .filter { predicate.invoke(it.value) }
-        .forEach { moveUpAt(it.index) }
+/** Moves all the elements that satisfies the given [predicate] up by an index, unless they are already at the top. */
+inline fun <T> MutableList<T>.moveUpAll(crossinline predicate: (T) -> Boolean): Unit = asSequence().withIndex()
+    .filter { predicate(it.value) }
+    .forEach { moveUpAt(it.index) }
 
-/**
- * Moves all **T** elements down an index that satisfy the given **predicate**, unless they are already at the bottom
- */
-inline fun <T> MutableList<T>.moveDownAll(crossinline predicate: (T) -> Boolean) = asSequence().withIndex()
-        .filter { predicate.invoke(it.value) }
-        .forEach { moveDownAt(it.index) }
+/** Moves all the elements that satisfies the given [predicate] down by an index, unless they are already at the bottom. */
+inline fun <T> MutableList<T>.moveDownAll(crossinline predicate: (T) -> Boolean): Unit = asSequence().withIndex()
+    .filter { predicate(it.value) }
+    .forEach { moveDownAt(it.index) }
 
 
-fun <T> MutableList<T>.moveToTopWhere(predicate: (T) -> Boolean) {
-    asSequence().filter(predicate).toList().asSequence().forEach {
-        remove(it)
-        add(0, it)
-    }
+fun <T> MutableList<T>.moveToTopWhere(predicate: (T) -> Boolean): Unit = asSequence().filter(predicate).forEach {
+    remove(it)
+    add(0, it)
 }
 
-fun <T> MutableList<T>.moveToBottomWhere(predicate: (T) -> Boolean) {
-    val end = size - 1
-    asSequence().filter(predicate).toList().asSequence().forEach {
-        remove(it)
-        add(end, it)
-    }
+fun <T> MutableList<T>.moveToBottomWhere(predicate: (T) -> Boolean): Unit = asSequence().filter(predicate).forEach {
+    remove(it)
+    add(lastIndex, it)
 }
 
 
-/**
- * Swaps the position of two items at two respective indices
- */
-fun <T> MutableList<T>.swap(indexOne: Int, indexTwo: Int) {
-    Collections.swap(this, indexOne, indexTwo)
-}
+/** Swaps the position of two items at two respective indices. */
+fun <T> MutableList<T>.swap(indexOne: Int, indexTwo: Int): Unit = Collections.swap(this, indexOne, indexTwo)
 
-/**
- * Swaps the index position of two items
- */
-fun <T> MutableList<T>.swap(itemOne: T, itemTwo: T) = swap(indexOf(itemOne), indexOf(itemTwo))
+/** Swaps the index position of two items */
+fun <T> MutableList<T>.swap(itemOne: T, itemTwo: T): Unit = swap(indexOf(itemOne), indexOf(itemTwo))
+
 
 /**
  * Bind this list to the given observable list by converting them into the correct type via the given converter.
  * Changes to the observable list are synced.
  */
-fun <SourceType, TargetType> MutableList<TargetType>.bind(sourceList: ObservableList<SourceType>, converter: (SourceType) -> TargetType): ListConversionListener<SourceType, TargetType> {
+fun <SourceType, TargetType> MutableList<TargetType>.bind(
+    sourceList: ObservableList<SourceType>,
+    converter: (SourceType) -> TargetType
+): ListConversionListener<SourceType, TargetType> {
     val ignoringParentConverter: (SourceType) -> TargetType = {
         FX.ignoreParentBuilder = Once
         try {
@@ -166,7 +141,7 @@ fun <SourceType, TargetType> MutableList<TargetType>.bind(sourceList: Observable
         }
     }
     val listener = ListConversionListener(this, ignoringParentConverter)
-    (this as?  ObservableList<TargetType>)?.setAll(sourceList.map(ignoringParentConverter)) ?: run {
+    (this as? ObservableList<TargetType>)?.setAll(sourceList.map(ignoringParentConverter)) ?: run {
         clear()
         addAll(sourceList.map(ignoringParentConverter))
     }
@@ -179,7 +154,10 @@ fun <SourceType, TargetType> MutableList<TargetType>.bind(sourceList: Observable
  * Bind this list to the given observable list by converting them into the correct type via the given converter.
  * Changes to the observable list are synced.
  */
-fun <SourceType, TargetType> MutableList<TargetType>.bind(sourceSet: ObservableSet<SourceType>, converter: (SourceType) -> TargetType): SetConversionListener<SourceType, TargetType> {
+fun <SourceType, TargetType> MutableList<TargetType>.bind(
+    sourceSet: ObservableSet<SourceType>,
+    converter: (SourceType) -> TargetType
+): SetConversionListener<SourceType, TargetType> {
     val ignoringParentConverter: (SourceType) -> TargetType = {
         FX.ignoreParentBuilder = Once
         try {
@@ -190,10 +168,7 @@ fun <SourceType, TargetType> MutableList<TargetType>.bind(sourceSet: ObservableS
     }
     val listener = SetConversionListener(this, ignoringParentConverter)
     if (this is ObservableList<*>) {
-        sourceSet.forEach { source ->
-            val converted = ignoringParentConverter(source)
-            listener.sourceToTarget[source] = converted
-        }
+        sourceSet.forEach { source -> listener.sourceToTarget[source] = ignoringParentConverter(source) }
         (this as ObservableList<TargetType>).setAll(listener.sourceToTarget.values)
     } else {
         clear()
@@ -205,8 +180,8 @@ fun <SourceType, TargetType> MutableList<TargetType>.bind(sourceSet: ObservableS
 }
 
 fun <SourceTypeKey, SourceTypeValue, TargetType> MutableList<TargetType>.bind(
-        sourceMap: ObservableMap<SourceTypeKey, SourceTypeValue>,
-        converter: (SourceTypeKey, SourceTypeValue) -> TargetType
+    sourceMap: ObservableMap<SourceTypeKey, SourceTypeValue>,
+    converter: (SourceTypeKey, SourceTypeValue) -> TargetType
 ): MapConversionListener<SourceTypeKey, SourceTypeValue, TargetType> {
     val ignoringParentConverter: (SourceTypeKey, SourceTypeValue) -> TargetType = { key, value ->
         FX.ignoreParentBuilder = FX.IgnoreParentBuilder.Once
@@ -218,14 +193,11 @@ fun <SourceTypeKey, SourceTypeValue, TargetType> MutableList<TargetType>.bind(
     }
     val listener = MapConversionListener(this, ignoringParentConverter)
     if (this is ObservableList<*>) {
-        sourceMap.forEach { source ->
-            val converted = ignoringParentConverter(source.key,source.value)
-            listener.sourceToTarget[source] = converted
-        }
+        sourceMap.forEach { source -> listener.sourceToTarget[source] = ignoringParentConverter(source.key, source.value) }
         (this as ObservableList<TargetType>).setAll(listener.sourceToTarget.values)
     } else {
         clear()
-        addAll(sourceMap.map{ignoringParentConverter(it.key, it.value) })
+        addAll(sourceMap.map { ignoringParentConverter(it.key, it.value) })
     }
     sourceMap.removeListener(listener)
     sourceMap.addListener(listener)
@@ -237,7 +209,11 @@ fun <SourceTypeKey, SourceTypeValue, TargetType> MutableList<TargetType>.bind(
  * Listens to changes on a list of SourceType and keeps the target list in sync by converting
  * each object into the TargetType via the supplied converter.
  */
-class ListConversionListener<SourceType, TargetType>(targetList: MutableList<TargetType>, val converter: (SourceType) -> TargetType) : ListChangeListener<SourceType>, WeakListener {
+class ListConversionListener<SourceType, TargetType>(
+    targetList: MutableList<TargetType>,
+    val converter: (SourceType) -> TargetType
+) : ListChangeListener<SourceType>, WeakListener {
+
     internal val targetRef: WeakReference<MutableList<TargetType>> = WeakReference(targetList)
 
     override fun onChanged(change: ListChangeListener.Change<out SourceType>) {
@@ -250,85 +226,34 @@ class ListConversionListener<SourceType, TargetType>(targetList: MutableList<Tar
                     list.subList(change.from, change.to).clear()
                     list.addAll(change.from, change.list.subList(change.from, change.to).map(converter))
                 } else {
-                    if (change.wasRemoved()) {
-                        list.subList(change.from, change.from + change.removedSize).clear()
-                    }
-                    if (change.wasAdded()) {
-                        list.addAll(change.from, change.addedSubList.map(converter))
-                    }
+                    if (change.wasRemoved()) list.subList(change.from, change.from + change.removedSize).clear()
+                    if (change.wasAdded()) list.addAll(change.from, change.addedSubList.map(converter))
                 }
             }
         }
     }
 
-    override fun wasGarbageCollected() = targetRef.get() == null
-
-    override fun hashCode() = targetRef.get()?.hashCode() ?: 0
+    override fun wasGarbageCollected(): Boolean = targetRef.get() == null
 
     override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
+        if (this === other) return true
+        if (other !is ListConversionListener<*, *>) return false
 
-        val ourList = targetRef.get() ?: return false
-
-        if (other is ListConversionListener<*, *>) {
-            val otherList = other.targetRef.get()
-            return ourList === otherList
-        }
-        return false
-    }
-}
-
-/**
- * Listens to changes on a Map of SourceTypeKey to SourceTypeValue and keeps the target list in sync by converting
- * each object into the TargetType via the supplied converter.
- */
-class MapConversionListener<SourceTypeKey, SourceTypeValue, TargetType>(
-        targetList: MutableList<TargetType>,
-        val converter: (SourceTypeKey, SourceTypeValue) -> TargetType
-) : MapChangeListener<SourceTypeKey, SourceTypeValue>, WeakListener {
-
-    internal val targetRef: WeakReference<MutableList<TargetType>> = WeakReference(targetList)
-    internal val sourceToTarget = HashMap<Map.Entry<SourceTypeKey, SourceTypeValue>, TargetType>()
-    override fun onChanged(change: MapChangeListener.Change<out SourceTypeKey, out SourceTypeValue>) {
-        val list = targetRef.get()
-        if (list == null) {
-            change.map.removeListener(this)
-        } else {
-            if (change.wasRemoved()) {
-                list.remove(converter(change.key, change.valueRemoved))
-            }
-            if (change.wasAdded()) {
-                list.add(converter(change.key, change.valueAdded))
-            }
-        }
+        val thisList = targetRef.get() ?: return false
+        return thisList === other.targetRef.get()
     }
 
-    override fun wasGarbageCollected() = targetRef.get() == null
-
-    override fun hashCode() = targetRef.get()?.hashCode() ?: 0
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-
-        val ourList = targetRef.get() ?: return false
-
-        if (other is MapConversionListener<*, *, *>) {
-            val otherList = other.targetRef.get()
-            return ourList === otherList
-        }
-        return false
-    }
+    override fun hashCode(): Int = targetRef.get()?.hashCode() ?: 0
 }
 
 /**
  * Listens to changes on a set of SourceType and keeps the target list in sync by converting
  * each object into the TargetType via the supplied converter.
  */
-class SetConversionListener<SourceType, TargetType>(targetList: MutableList<TargetType>, val converter: (SourceType) -> TargetType) : SetChangeListener<SourceType>, WeakListener {
+class SetConversionListener<SourceType, TargetType>(
+    targetList: MutableList<TargetType>,
+    val converter: (SourceType) -> TargetType
+) : SetChangeListener<SourceType>, WeakListener {
     internal val targetRef: WeakReference<MutableList<TargetType>> = WeakReference(targetList)
     internal val sourceToTarget = HashMap<SourceType, TargetType>()
 
@@ -350,28 +275,57 @@ class SetConversionListener<SourceType, TargetType>(targetList: MutableList<Targ
         }
     }
 
-    override fun wasGarbageCollected() = targetRef.get() == null
-
-    override fun hashCode() = targetRef.get()?.hashCode() ?: 0
+    override fun wasGarbageCollected(): Boolean = targetRef.get() == null
 
     override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
+        if (this === other) return true
+        if (other !is SetConversionListener<*, *>) return false
 
-        val ourList = targetRef.get() ?: return false
-
-        if (other is SetConversionListener<*, *>) {
-            val otherList = other.targetRef.get()
-            return ourList === otherList
-        }
-        return false
+        val thisList = targetRef.get() ?: return false
+        return thisList === other.targetRef.get()
     }
+
+    override fun hashCode(): Int = targetRef.get()?.hashCode() ?: 0
 }
+
+/**
+ * Listens to changes on a Map of SourceTypeKey to SourceTypeValue and keeps the target list in sync by converting
+ * each object into the TargetType via the supplied converter.
+ */
+class MapConversionListener<SourceTypeKey, SourceTypeValue, TargetType>(
+    targetList: MutableList<TargetType>,
+    val converter: (SourceTypeKey, SourceTypeValue) -> TargetType
+) : MapChangeListener<SourceTypeKey, SourceTypeValue>, WeakListener {
+
+    internal val targetRef: WeakReference<MutableList<TargetType>> = WeakReference(targetList)
+    internal val sourceToTarget = HashMap<Map.Entry<SourceTypeKey, SourceTypeValue>, TargetType>()
+
+    override fun onChanged(change: MapChangeListener.Change<out SourceTypeKey, out SourceTypeValue>) {
+        val list = targetRef.get()
+        if (list == null) {
+            change.map.removeListener(this)
+        } else {
+            if (change.wasRemoved()) list.remove(converter(change.key, change.valueRemoved))
+            if (change.wasAdded()) list.add(converter(change.key, change.valueAdded))
+        }
+    }
+
+    override fun wasGarbageCollected(): Boolean = targetRef.get() == null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is MapConversionListener<*, *, *>) return false
+
+        val thisList = targetRef.get() ?: return false
+        return thisList === other.targetRef.get()
+    }
+
+    override fun hashCode(): Int = targetRef.get()?.hashCode() ?: 0
+}
+
 
 fun <T> ObservableList<T>.invalidate() {
     if (isNotEmpty()) this[0] = this[0]
 }
 
-
-fun <T> observableList(vararg entries: T) : ObservableList<T> = FXCollections.observableArrayList<T>(entries.toList())
+fun <T> observableList(vararg entries: T): ObservableList<T> = FXCollections.observableArrayList<T>(entries.toList())
