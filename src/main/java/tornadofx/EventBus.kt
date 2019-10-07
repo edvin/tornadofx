7580ecb861a2821c7cc5acb5efa90da387251121
Sch.Funtik <schweinchenfuntik@gmail.com>
2019-10-07 22:47:38 +0300

    partially fix EventBus

--- src/main/java/tornadofx/EventBus.kt
+++ src/main/java/tornadofx/EventBus.kt
@@ -4,9 +4,10 @@ package tornadofx
 
 import javafx.application.Platform
 import tornadofx.EventBus.RunOn.ApplicationThread
-import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicLong
 import java.util.logging.Level
+import kotlin.collections.HashSet
 import kotlin.concurrent.thread
 import kotlin.reflect.KClass
 
@@ -22,43 +23,67 @@ class EventContext {
     }
 }
 
-class FXEventRegistration(val eventType: KClass<out FXEvent>, val owner: Component?, val maxCount: Long? = null, val action: EventContext.(FXEvent) -> Unit) {
-    val count = AtomicLong()
+interface EventRegistration {
+    val eventType: KClass<out FXEvent>
+    val owner: Component?
+    val action: EventContext.(FXEvent) -> Unit
+    fun unsubscribe()
+}
+
+class FXEventRegistration(override val eventType: KClass<out FXEvent>,
+                          override val owner: Component?,
+                          private val maxCount: Long? = null,
+                          private val anAction: EventContext.(FXEvent) -> Unit) : EventRegistration {
+
+    private val invocationCount = AtomicLong()
+
+    override val action: EventContext.(FXEvent) -> Unit = {
+        if (maxCount == null || invocationCount.getAndIncrement() < maxCount) {
+            anAction.invoke(this, it)
+        } else {
+            this@FXEventRegistration.unsubscribe()
+        }
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other?.javaClass != javaClass) return false
-
-        other as FXEventRegistration
-
+        if (other !is FXEventRegistration) return false
         if (eventType != other.eventType) return false
         if (owner != other.owner) return false
+        if (maxCount != other.maxCount) return false
         if (action != other.action) return false
+        if (invocationCount.get() != other.invocationCount.get()) return false
 
         return true
     }
 
     override fun hashCode(): Int {
         var result = eventType.hashCode()
-        result = 31 * result + (owner?.hashCode() ?: 0)
+        result = 31 * result + owner.hashCode()
         result = 31 * result + action.hashCode()
         return result
     }
 
-    fun unsubscribe() {
+    override fun unsubscribe() {
         FX.eventbus.unsubscribe(this)
     }
 }
 
+data class InvalidatableEventRegistration(val registration: EventRegistration, @Volatile var valid: Boolean = true) : EventRegistration by registration
+
 class EventBus {
     enum class RunOn { ApplicationThread, BackgroundThread }
 
-    private val subscriptions = mutableMapOf<KClass<out FXEvent>, HashSet<FXEventRegistration>>()
-    private val eventScopes = mutableMapOf<EventContext.(FXEvent) -> Unit, Scope>()
+    private val subscriptions = ConcurrentHashMap<KClass<out FXEvent>, Set<InvalidatableEventRegistration>>()
+    private val eventScopes = ConcurrentHashMap<EventContext.(FXEvent) -> Unit, Scope>()
 
     inline fun <reified T : FXEvent> subscribe(scope: Scope, registration: FXEventRegistration) = subscribe(T::class, scope, registration)
     fun <T : FXEvent> subscribe(event: KClass<T>, scope: Scope, registration: FXEventRegistration) {
-        subscriptions.getOrPut(event, { HashSet() }).add(registration)
+        subscriptions.compute(event) { _, set ->
+            val newSet = if (set != null) HashSet(set.filter { it.valid }) else HashSet()
+            newSet.add(InvalidatableEventRegistration(registration))
+            newSet
+        }
         eventScopes[registration.action] = scope
     }
 
@@ -74,33 +99,38 @@ class EventBus {
     fun <T : FXEvent> unsubscribe(event: Class<T>, action: EventContext.(T) -> Unit) = unsubscribe(event.kotlin, action)
 
     fun <T : FXEvent> unsubscribe(event: KClass<T>, action: EventContext.(T) -> Unit) {
-        subscriptions[event]?.removeAll { it.action == action }
+        subscriptions[event]?.forEach {
+            if (it.action == action) {
+                it.valid = false
+            }
+        }
         eventScopes.remove(action)
     }
 
-    fun unsubscribe(registration: FXEventRegistration) {
+    fun unsubscribe(registration: EventRegistration) {
         unsubscribe(registration.eventType, registration.action)
         registration.owner?.subscribedEvents?.get(registration.eventType)?.remove(registration)
     }
 
+    private fun cleanupInvalidRegistrations(eventKlass: KClass<FXEvent>) {
+        subscriptions.computeIfPresent(eventKlass) { _, set -> set.filter { it.valid }.toSet() }
+    }
+
     fun fire(event: FXEvent) {
         fun fireEvents() {
-            subscriptions[event.javaClass.kotlin]?.forEach {
-                if (event.scope == null || event.scope == eventScopes[it.action]) {
-                    val count = it.count.andIncrement
-                    if (it.maxCount == null || count < it.maxCount) {
-                        val context = EventContext()
-                        try {
-                            it.action.invoke(context, event)
-                        } catch (subscriberFailure: Exception) {
-                            log.log(Level.WARNING, "Event $event was delivered to subscriber from ${it.owner}, but invocation resulted in exception", subscriberFailure)
-                        }
-                        if (context.unsubscribe) unsubscribe(it)
-                    } else {
-                        unsubscribe(it)
+            val eventKlass = event.javaClass.kotlin
+            subscriptions[eventKlass]?.forEach {
+                if (it.valid && (event.scope == null || event.scope == eventScopes[it.action])) {
+                    val context = EventContext()
+                    try {
+                        it.action.invoke(context, event)
+                    } catch (subscriberFailure: Exception) {
+                        log.log(Level.WARNING, "Event $event was delivered to subscriber from ${it.owner}, but invocation resulted in exception", subscriberFailure)
                     }
+                    if (context.unsubscribe) unsubscribe(it)
                 }
             }
+            cleanupInvalidRegistrations(eventKlass)
         }
 
         if (Platform.isFxApplicationThread()) {
@@ -120,6 +150,20 @@ class EventBus {
                 fireEvents()
             }
         }
+    }
+
+    internal fun clear() {
+        subscriptions.clear()
+        eventScopes.clear()
+    }
+
+    internal fun unsubscribeAll(scope: Scope) {
+        val scopedContexts: Map<EventContext.(FXEvent) -> Unit, Scope> = eventScopes.filter { it.value == scope }
+        val registrations = mutableListOf<EventRegistration>()
+        subscriptions.forEach { (_, subscriptionRegistrations) ->
+            registrations += subscriptionRegistrations.filter { scopedContexts.containsKey(it.action) }
+        }
+        registrations.forEach { it.unsubscribe() }
     }
 
 }
